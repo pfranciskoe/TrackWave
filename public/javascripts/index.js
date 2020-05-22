667eb9096e6958f0bf8792608030638e235df45d
peter <pfranciskoe@gmail.com>
2020-05-21 17:05:25 -0700

    updated rendering of pitch

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,19 +5,25 @@ document.addEventListener('DOMContentLoaded', () => {
     canvas = document.getElementById('track-canvas');
     ctx = canvas.getContext('2d')
 
-    let trackId = '2cmRpmO04TLaKPzmAzySYZ';
+    let trackId = '5f0LVeG7bQD0p20iSqA9F0';
     axios.get(`/tracks/${trackId}`)
     .then((response) => {
         
         const spotifyTrack = response;
         console.log(spotifyTrack);
         spotifyTrack.data.segments.forEach((segment,idx) => {
+            
             if ((idx % 2) === 0) { ctx.fillStyle = '#a2cdf5'} else {
                 ctx.fillStyle = '#0373fc'
             }
-            ctx.beginPath()
-            ctx.arc(10*(segment.start + (segment.duration / 2)), 500 + segment.loudness_start, 10*(segment.duration/2), 0, 2 * Math.PI, false)
-            ctx.fill()
+            segment.pitches.forEach((pitchPoint,idx) =>{
+                if (idx === 0 ){
+                ctx.beginPath()
+                ctx.arc(2*(segment.start + (segment.duration / 2)), 500 * pitchPoint, 2*(segment.duration/2), 0, 2 * Math.PI, false)
+                ctx.fill()
+                }
+            })
+
         })
 
     })
